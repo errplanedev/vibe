af1c7bb79ae1950e3f7b823f68bc2b8e472d4270
errplane <110797323+errplanedev@users.noreply.github.com>
2024-03-20 18:08:15 -0700

    postgres

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -3,7 +3,7 @@ import { post } from "@/server/post";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  const thePost = await post(req.headers.get('Authorization'), body.content);
+  const thePost = await post(req.headers.get('Authorization')!, body.content);
 
   return Response.json(thePost);
 }
